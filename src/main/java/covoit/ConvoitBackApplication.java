4dd8cbc8ccde1f5dc370381ad71a010c540afad6
frederic smée <ssdfred@yahoo.fr>
2024-07-16 11:38:03 +0200

    initialisation

---
+++ src/main/java/covoit/ConvoitBackApplication.java
@@ -0,0 +1,13 @@
+package covoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvoitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvoitBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/covoit/ConvoitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package covoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvoitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
