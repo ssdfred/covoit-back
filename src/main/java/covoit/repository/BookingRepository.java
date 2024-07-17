69e492961e0636aaac40949cac09d3e9e27a1a1c
MiL-Dev <mil.devacc@gmail.com>
2024-07-17 14:39:30 +0200

    BookingService and Repository

---
+++ src/main/java/covoit/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package covoit.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import covoit.entities.Booking;
+
+public interface BookingRepository extends CrudRepository<Booking, Integer> {
+
+}
