048b5b106ab2e8404ee1e0ca6fa5990e21ffcca9
MiL-Dev <mil.devacc@gmail.com>
2024-07-17 14:36:21 +0200

    RouteService and Repository

---
+++ src/main/java/covoit/repository/RouteRepository.java
@@ -0,0 +1,9 @@
+package covoit.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import covoit.entities.Route;
+
+public interface RouteRepository extends CrudRepository<Route, Integer> {
+
+}
