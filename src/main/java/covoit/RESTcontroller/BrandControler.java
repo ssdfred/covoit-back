d6ca0dd3080408f108b0806dbae34c50d47c3ee6
Noémie Letenneur <n.letenneur@gmail.com>
2024-07-22 12:41:14 +0200

    Refactor : BrandControler #138

--- src/main/java/covoit/RESTcontroller/BrandControler.java
+++ src/main/java/covoit/RESTcontroller/BrandControler.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import covoit.entities.Booking;
 import covoit.entities.Brand;
 import covoit.services.BrandService;
 
