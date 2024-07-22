bc14239ddebf22626d539887954b9d29e7662216
Noémie Letenneur <n.letenneur@gmail.com>
2024-07-22 13:57:22 +0200

    Refactor : UserAccountService #140

--- src/main/java/covoit/services/UserAccountService.java
+++ src/main/java/covoit/services/UserAccountService.java
@@ -1,17 +1,13 @@
 
 package covoit.services;
 
-import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import covoit.dtos.UserAccountDto;
-import covoit.entities.Brand;
 import covoit.entities.UserAccount;
-import covoit.repository.CarpoolRepository;
 import covoit.repository.UserAccountRepository;
 
 /**
