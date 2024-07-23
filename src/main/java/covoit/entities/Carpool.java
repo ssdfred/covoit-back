699b2d7e09dace0f10f239f7a22a70ad52cc737b
Noémie Letenneur <n.letenneur@gmail.com>
2024-07-23 15:42:14 +0200

    Refactor to Dto using - Carpool #149

--- src/main/java/covoit/entities/Carpool.java
+++ src/main/java/covoit/entities/Carpool.java
@@ -18,124 +18,118 @@ import jakarta.persistence.ManyToOne;
  */
 @Entity
 public class Carpool {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private int availableSeat;
-    private LocalDate startDate;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int availableSeat;
+	private LocalDate startDate;
 
-    @ManyToOne
-    private Vehicle vehicule;
+	@ManyToOne
+	private Vehicle vehicule;
 
-    @ManyToOne
-    private Route route;
-    
-    @ManyToMany
-    @JoinTable(name = "useraccount_carpoll", joinColumns = @JoinColumn(name = "id_carpool"), inverseJoinColumns = @JoinColumn(name = "id_userAccount"))
-    private List<UserAccount> userAccounts = new ArrayList<>();
+	@ManyToOne
+	private Route route;
 
-    public Carpool() {
-	
-    }
+	@ManyToMany
+	@JoinTable(name = "useraccount_carpoll", joinColumns = @JoinColumn(name = "id_carpool"), inverseJoinColumns = @JoinColumn(name = "id_userAccount"))
+	private List<UserAccount> userAccounts = new ArrayList<>();
 
+	public Carpool() {
 
+	}
 
-    /**
-     * Gets the unique identifier of the reservation.
-     * 
-     * @return the unique identifier of the reservation
-     */
-    public Long getId() {
-	return id;
-    }
+	/**
+	 * Gets the unique identifier of the reservation.
+	 * 
+	 * @return the unique identifier of the reservation
+	 */
+	public int getId() {
+		return id;
+	}
 
-    /**
-     * Sets the unique identifier of the reservation.
-     * 
-     * @param id the unique identifier of the reservation
-     */
-    public void setId(Long id) {
-	this.id = id;
-    }
+	/**
+	 * Sets the unique identifier of the reservation.
+	 * 
+	 * @param id the unique identifier of the reservation
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public int getAvailableSeat() {
-	return availableSeat;
-    }
+	public int getAvailableSeat() {
+		return availableSeat;
+	}
 
-    public void setAvailableSeat(int availableSeat) {
-	this.availableSeat = availableSeat;
-    }
+	public void setAvailableSeat(int availableSeat) {
+		this.availableSeat = availableSeat;
+	}
 
-    /**
-     * Gets the start date and time of the reservation.
-     * 
-     * @return the start date and time of the reservation
-     */
-    public LocalDate getStartDate() {
-	return startDate;
-    }
+	/**
+	 * Gets the start date and time of the reservation.
+	 * 
+	 * @return the start date and time of the reservation
+	 */
+	public LocalDate getStartDate() {
+		return startDate;
+	}
 
-    /**
-     * Sets the start date and time of the reservation.
-     * 
-     * @param startDateTime the start date and time of the reservation
-     */
-    public void setStartDate(LocalDate startDate) {
-	this.startDate = startDate;
-    }
+	/**
+	 * Sets the start date and time of the reservation.
+	 * 
+	 * @param startDateTime the start date and time of the reservation
+	 */
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
 
+	/**
+	 * @return
+	 */
+	public Route getRoute() {
+		return route;
+	}
 
-    /**
-     * @return
-     */
-    public Route getRoute() {
-        return route;
-    }
+	/**
+	 * @param route
+	 */
+	public void setRoute(Route route) {
+		this.route = route;
+	}
 
-    /**
-     * @param route
-     */
-    public void setRoute(Route route) {
-        this.route = route;
-    }
-    /**
-     * Sets the user who made the reservation.
-     * 
-     * @param user the user who made the reservation
-     */
-    public void setUserAccounts(List<UserAccount> userAccounts) {
-        this.userAccounts = userAccounts;
-    }
+	/**
+	 * Sets the user who made the reservation.
+	 * 
+	 * @param user the user who made the reservation
+	 */
+	public void setUserAccounts(List<UserAccount> userAccounts) {
+		this.userAccounts = userAccounts;
+	}
 
-    /**
-     * Gets the user who made the reservation.
-     * 
-     * @return the user who made the reservation
-     */
-    public List<UserAccount> getUserAccounts() {
-        return userAccounts;
-    }
+	/**
+	 * Gets the user who made the reservation.
+	 * 
+	 * @return the user who made the reservation
+	 */
+	public List<UserAccount> getUserAccounts() {
+		return userAccounts;
+	}
 
+	/**
+	 * Gets the vehicle associated with the reservation.
+	 * 
+	 * @return the vehicle associated with the reservation
+	 */
+	public Vehicle getVehicle() {
+		return vehicule;
+	}
 
-
-    /**
-     * Gets the vehicle associated with the reservation.
-     * 
-     * @return the vehicle associated with the reservation
-     */
-    public Vehicle getVehicle() {
-	return vehicule;
-    }
-
-    /**
-     * Sets the vehicle associated with the reservation.
-     * 
-     * @param vehicle the vehicle associated with the reservation
-     */
-    public void setVehicle(Vehicle vehicule) {
-	this.vehicule = vehicule;
-    }
-
-
+	/**
+	 * Sets the vehicle associated with the reservation.
+	 * 
+	 * @param vehicle the vehicle associated with the reservation
+	 */
+	public void setVehicle(Vehicle vehicule) {
+		this.vehicule = vehicule;
+	}
 
 }
