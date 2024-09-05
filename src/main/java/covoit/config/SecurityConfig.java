6753c836edb695030c4d7a5b48ac64a06b68c2c9
ssdfred <ssdfred@yahoo.fr>
2024-09-05 15:30:49 +0200

    cookies

--- src/main/java/covoit/config/SecurityConfig.java
+++ src/main/java/covoit/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 				.requestMatchers("/user/delete/**").hasRole("ADMIN")
 				.anyRequest().authenticated())
 			.httpBasic(Customizer.withDefaults())
-			.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository)) // Désactiver CSRF pour REST
+			.csrf(csrf -> csrf.csrfTokenRepository()) 
 			.headers(headers -> headers.contentSecurityPolicy("default-src 'self'"));
 		
 		return http.build();
