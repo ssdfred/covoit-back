f0e24ecfe05f04e6edaa4029fb44575ec64c1673
ssdfred <ssdfred@yahoo.fr>
2024-09-05 13:04:18 +0200

    chore: Update CSRF token repository configuration in SecurityConfig.java

--- src/main/java/covoit/config/SecurityConfig.java
+++ src/main/java/covoit/config/SecurityConfig.java
@@ -7,35 +7,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import ch.qos.logback.core.Context;
 import covoit.entities.UserAccount;
 import covoit.repository.UserAccountRepository;
-import javax.servlet.http.Cookie;
+
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
 	@Bean
 	public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
-    // Créer un dépôt de contexte de sécurité pour stocker la session
-    HttpSessionSecurityContextRepository repo = new HttpSessionSecurityContextRepository();
-    
-
+		HttpSessionSecurityContextRepository repo = new HttpSessionSecurityContextRepository();
 		http.authorizeHttpRequests(
 				(request) -> request.requestMatchers("/user/", "/user/register", "auth/login", "/**", "/swagger-ui/")
 						.permitAll().requestMatchers("/user/{id}").hasRole("USER")
 						.requestMatchers("/**", "/user/delete/**").hasRole("ADMIN").anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults())
-				.securityContext((context -> context.securityContextRepository(repo))); // Utiliser le dépôt de contexte
-				// Configurer la protection CSRF avec un cookie sécurisé (HttpOnly est activé par défaut)
-				http.csrf(csrf -> csrf
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyTrue())); // Désactive HttpOnly uniquement si nécessaire
+				.securityContext((context -> context.securityContextRepository(repo)));
+		// Configurer CSRF avec CookieCsrfTokenRepository et HttpOnly désactivé
+        http.csrf(csrf -> csrf
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
 
 
-
+		//http.csrf(csrf -> csrf.disable());
 		return http.build();
 	}
 
@@ -46,18 +48,27 @@ public class SecurityConfig implements WebMvcConfigurer {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("http://localhost:4200")
 						.allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
-
+				// .allowCredentials(true);
 			}
 		};
 	}
-
+	// Creation UserDetail temporaire
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsService() {
+//		UserDetails user = User.builder().username("user").password(passwordEncoder().encode("user")).roles("USER")
+//				.build();
+//		UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
+//				.build();
+//		System.out.println(user.getAuthorities());
+//		return new InMemoryUserDetailsManager(user, admin);
+//	}
 
 	@Bean
 	public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
 		return username -> {
 			UserAccount userAccount = userAccountRepository.findByUserName(username);
 			if (userAccount == null) {
-				
+				throw new UsernameNotFoundException("User not found");
 			}
 			return userAccount.asUserDetails(); // Assuming `asUserDetails()` converts `UserAccount` to `UserDetails`
 		};
